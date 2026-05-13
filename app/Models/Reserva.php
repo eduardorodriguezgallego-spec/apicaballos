359cfed19be3a63316be59450f84e8059b14c813
root <root@vmi3217805.contaboserver.net>
2026-05-14 00:52:14 +0200

    Corrige relacion Reserva con Usuario

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -21,7 +21,7 @@ class Reserva extends Model
 
     public function usuario()
     {
-        return $this->belongsTo(User::class, 'usuario_id');
+        return $this->belongsTo(Usuario::class, 'usuario_id');
     }
 
     public function caballo()
